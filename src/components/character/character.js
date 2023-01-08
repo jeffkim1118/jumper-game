4b2131c8a97db89e842625a129f008937546040c
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-01-08 02:18:51 -0500

    added arrow key functions for each keys

--- src/components/character/character.js
+++ src/components/character/character.js
@@ -1,19 +1,43 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Jumper from '../../images/character.jpg';
-import Movement from './movement';
+
 
 function Character(){
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    let jumper = document.getElementsByClassName("jumper")
+    
+    function moveRight(){
+        jumper.style.left = parseInt(jumper.style.left) + 5 + "px";
+    }
+
+    function moveLeft(){
+        jumper.style.left = parseInt(jumper.style.left) - 5 + "px";
+    }
+
+    function makeJump(){
+        jumper.style.top = parseInt(jumper.style.top) - 5 + "px"
+    }
+
+
     useEffect(() => {
         document.addEventListener('keydown', detectKeyDown, true)
     }, [])
 
     const detectKeyDown = (e) => {
-       if(e.key){<Movement direction={e.key}/>}
+       if(e.key === "ArrowRight"){
+        moveRight();
+       }else if(e.key === "ArrowLeft"){
+        moveLeft();
+       }else if(e.key === "ArrowUp" || " "){
+        makeJump()
+       }
     }
     
     return (
         <div>
-            <img className="jumper" src={Jumper} alt="jumper"/>
+            <img className="jumper" style={{top:"80vh", left:"0px", right:"0px", position:"relative"}} src={Jumper} alt="jumper"/>
         </div>
     )
 }
