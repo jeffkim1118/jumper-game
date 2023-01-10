431d86cdc3b2b112d85b81f6fdc454ebff17d512
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2023-01-10 16:22:04 -0500

    attempting to create multiple platforms using map

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Plane from './components/obstacles/plane';
 import Platform from './components/platform/platform'
 
 function App() {
-  const numOfPlatform = [400,500,600,700]
+  const heightsOfPlatform = [400,500,600,700]
+  const dataComp = heightsOfPlatform.map((height) => {
+    return (
+      <Platform key={height} />
+    )
+  })
+
   return (
     <div className="App">
       <Player />
       <Bird />
       <Plane />
-      {numOfPlatform.map(i => {
-        return <Platform/>
-      })}
+      {dataComp}
     </div>
   );
 }
--- src/components/platform/platform.js
+++ src/components/platform/platform.js
@@ -1,15 +1,20 @@
 import React from "react";
 
-class Platform extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {left: 400,top: 500}
-    }
-    render(){
-        
-        return(           
-            <div className="platform" style={{top:this.state.top+"px", left:this.state.left+"px"}}></div>
-        )
-    }
+// class Platform extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {left: 400, top: 500}
+//     }
+//     render(){
+//         return(           
+//             <div className="platform" style={{top:this.state.top+"px", left:this.state.left+"px"}}></div>
+//         )
+//     }
+// }
+function Platform({top}){
+    return(
+        <div className="platform" style={{left:200+"px", top:top}}></div>
+    )
 }
+
 export default Platform;
